6aa350a11c0a3f33926f5c6bcf53ab68d8bbe5c2
petya-pital <35997715+petya-pital@users.noreply.github.com>
2018-01-31 17:46:14 +0300

    Add files via upload

---
+++ t11.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 17:04:14 2018
+
+@author: Петя
+"""
+print(abs.__doc__)
+print(bool.__doc__)
---
+++ t12.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 17:10:09 2018
+
+@author: Петя
+"""
+
+import calendar
+print(calendar.month(int(input('press year\n')),int(input('press moth\n'))))
